cce5bc03d2728473c0735e92ea5f80251873a2ce
Shiva pandey <shivapandey9898@gmail.com>
2024-07-19 09:52:33 +0530

    feat: created link new window atom jsx

---
+++ src/atoms/links/new-window/jsx/index.jsx
@@ -0,0 +1,8 @@
+const LinkNewWindow = ({ aria_label, href, children }) => {
+    return (
+        <a href={href} aria-label={aria_label} target="_blank" rel="noopener noreferrer">
+            {children}
+        </a>
+    )
+}
+export default LinkNewWindow
