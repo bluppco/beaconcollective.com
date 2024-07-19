fb0aa49a5ccf04768d1707821cf615043b6dec44
Shiva pandey <shivapandey9898@gmail.com>
2024-07-19 09:52:17 +0530

    feat: created link atom for jsx

---
+++ src/atoms/links/jsx/index.jsx
@@ -0,0 +1,8 @@
+const Link = ({ aria_label, href, children }) => {
+    return (
+        <a href={href} aria-label={aria_label} rel="noopener noreferrer">
+            {children}
+        </a>
+    )
+}
+export default Link
