0c6117455e3a342cd5c054124444b3c45cdcf307
Shiva pandey <shivapandey9898@gmail.com>
2024-07-19 09:51:53 +0530

    feat: created h1 atom in jsx

---
+++ src/atoms/atoms-shiva/headings/one/six-xl/semibold/white/jsx/index.jsx
@@ -0,0 +1,8 @@
+const HeadingOneWHite = ({ children }) => {
+    return (
+        <h1 className="text-4xl lg:text-6xl font-semibold font-larken text-white">
+            {children}
+        </h1>
+    )
+}
+export default HeadingOneWHite
