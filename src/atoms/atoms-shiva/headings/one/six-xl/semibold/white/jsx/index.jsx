1b544125db0fcf15437294e1e01e9c1122c755b4
Shiva pandey <shivapandey9898@gmail.com>
2024-07-22 10:28:27 +0530

    refactor: rearanged tailwind properties

--- src/atoms/atoms-shiva/headings/one/six-xl/semibold/white/jsx/index.jsx
+++ src/atoms/atoms-shiva/headings/one/six-xl/semibold/white/jsx/index.jsx
@@ -1,8 +1,12 @@
-const HeadingOneWHite = ({ children }) => {
+const HeadingOneWHite = ( props ) => {
+
     return (
-        <h1 className="text-4xl lg:text-6xl font-semibold font-larken text-white">
-            {children}
+
+        <h1 className="text-4xl lg:text-6xl text-white font-semibold font-larken">
+            { props.children }
         </h1>
+
     )
+
 }
 export default HeadingOneWHite
