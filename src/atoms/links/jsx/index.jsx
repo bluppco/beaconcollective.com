93ab1957268a8ce327661df8bbd6b3a5399a1d47
Shiva pandey <shivapandey9898@gmail.com>
2024-07-23 08:48:54 +0530

    refactor: added space after curly brakets

--- src/atoms/links/jsx/index.jsx
+++ src/atoms/links/jsx/index.jsx
@@ -1,8 +1,12 @@
 const Link = ({ aria_label, href, children }) => {
+
     return (
-        <a href={href} aria-label={aria_label} rel="noopener noreferrer">
-            {children}
+
+        <a href={ href } aria-label={ aria_label } rel="noopener noreferrer">
+            { children }
         </a>
+
     )
+
 }
 export default Link
--- src/atoms/links/new-window/jsx/index.jsx
+++ src/atoms/links/new-window/jsx/index.jsx
@@ -1,8 +1,12 @@
 const LinkNewWindow = ({ aria_label, href, children }) => {
+
     return (
-        <a href={href} aria-label={aria_label} target="_blank" rel="noopener noreferrer">
-            {children}
+
+        <a href={ href } aria-label={ aria_label } target="_blank" rel="noopener noreferrer">
+            { children }
         </a>
+
     )
+
 }
 export default LinkNewWindow
